189ff3224ba989d9fb46a2c11c913fb7d5796577
rlba96 <57870939+rlba96@users.noreply.github.com>
2019-11-20 16:48:39 +0000

    Add files via upload

--- BITalino_SQLite.py
+++ BITalino_SQLite.py
@@ -3,6 +3,7 @@ import sqlite3
 import bitalino
 import time
 import numpy
+from scipy import signal
 
 print("BITalino Data Collection")
 time.sleep(2)
@@ -11,14 +12,14 @@ time.sleep(2)
 database = "data.db"
 #database = "C:\Users\User\Desktop\Teste\SQlite_example"
 
-# Device MacAddress: BLE = 88:6B:0F:94:45:D5, Blt = 20:15:12:22:81:68
-macAddress = "20:15:12:22:81:68"
+# Device MacAddress: Blt1 = 98:D3:81:FD:61:22, Blt2 = 20:15:12:22:81:68
+macAddress = "98:D3:81:FD:61:22"
 
 # Acquisition Channels ([0-5])
 acqChannels = [0,1,2,3,4,5]
 
 # Sampling Frequency (Hz)
-samplingFreq = 1000
+samplingFreq = 10
 
 # Compute Average Time (s)
 timeCycle = 1
@@ -50,16 +51,39 @@ database.commit()
 
 lastIndex = cursor.lastrowid
 print("Current Configuration ID: " + str(lastIndex))
+print("Collecting data.")
 
 currentTime = 0
 while (acquisitionTime is None) or (acquisitionTime > 0):
     avg_data = [lastIndex, currentTime, None, None, None, None, None, None]
     data = device.read(samplingFreq*timeCycle)
+    """
+    Values acquired per second:
+    	Frequency = 10Hz => 10 values
+    	Frequency = 30Hz => 30 values
+    	Frequency = 100Hz => 100 values
+    	...
+    	...
+    	data[:,5] => Channel 1 (on BITalino)
+    	data[:,6] => Channel 2
+    	data[:,7] => Channel 3
+    	data[:,8] => Channel 4
+    	data[:,9] => Channel 5
+    	data[:,10] => Channel 5
+    """
+    print("%=====================%")
+    print(data[:,5])	# acc data for channel 1
+    print("%=====================%")
+
+    """
+    Apply filter to acc data here
+    """
+
     for ind in range(5, data.shape[1]):
         avg_data[acqChannels[ind - 5] + 2] = numpy.mean(numpy.fabs(data[:,ind]))
         # print(ind)
         # Aply transfer functions here;
-    print("Acc: %s",avg_data[2])
+    print("Acc mean: = ", avg_data[2])
     cursor.execute("INSERT INTO Data(Configuration, Time, Channel0, Channel1, Channel2, Channel3, Channel4, Channel5) VALUES" +
                     "(" + str(avg_data).replace("None", "null")[1:-1] + ");")
     database.commit()
@@ -69,6 +93,7 @@ while (acquisitionTime is None) or (acquisitionTime > 0):
 
 device.stop()
 device.close()
+print("Connection closed.")
 
 # UnComment to Print Tables
 
