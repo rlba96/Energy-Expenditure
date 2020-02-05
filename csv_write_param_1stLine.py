1c303e3b090d8dbc78177a2b631e88e13bdde585
rlba96 <57870939+rlba96@users.noreply.github.com>
2020-02-05 17:11:14 +0000

    Delete csv_write_param_1stLine.py

--- csv_write_param_1stLine.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import csv
-
-with open('EE_Dataset_Jogging_v1_0.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Timestamp", "Name", "Weight", "Activity", "Acceleration", "Heart_Rate", "Oxygen_Saturation", "MET", "Caloric Expenditure"])
