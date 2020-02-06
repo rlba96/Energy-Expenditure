880040d84547651519cbb49d0bc08e61554e4569
rlba96 <57870939+rlba96@users.noreply.github.com>
2020-02-06 14:43:33 +0000

    Update dropRows.py

--- BITalino/dropRows.py
+++ BITalino/dropRows.py
@@ -5,7 +5,7 @@ data = pd.read_csv('output1.csv',usecols = ['timestamp', 'AccX', 'AccY', 'AccZ']
 
 df = pd.DataFrame(data) 
 	
-# Get names of indexes for which column timestamp has repetead values in column AccX
+# Get names of indexes for which column timestamp has repetead values in column AccZ
 indexNames = df[ df['timestamp'] == df['AccZ'] ].index
  
 # Delete these row indexes from dataFrame
