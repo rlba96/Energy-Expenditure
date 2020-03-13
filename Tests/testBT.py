5f734f1b038a0f5925edf26b3bf91f174d28afb8
rlba96 <57870939+rlba96@users.noreply.github.com>
2020-03-13 12:33:26 +0000

    Add files via upload

--- Tests/testBT.py
+++ Tests/testBT.py
@@ -2,67 +2,49 @@
 """
 A simple Python script to receive messages from a client over 
 Bluetooth using PyBluez (with Python 2). 
+
+print(seconds,data[7])
+print(seconds,',[',data[0],' ',data[1],' ',data[2],' ',data[3],' ',data[4],' ',data[5],' ',data[6],' ',data[7],' ',data[8],' ',data[9],' ',data[10],' ',data[11],' ]')
+    
+
 """
 import bluetooth
 import sys
 import time
 import csv
+import numpy as np
 
 start = int(time.time())
 end = int(time.time())
-runtime = 15
-bd_addr = "30:AE:A4:CC:26:12" #itade address
+runtime = 10
+bd_addr = "C4:4F:33:3E:B3:CB"   #top: 30:AE:A4:CC:26:12    low:C4:4F:33:3E:B3:CB
 port = 1
-vectorLen = 6
-sensitivity = 2048          # 2g-16384; 4g-8192; 8g-4096; 16g-2048; according to the datasheet
+vectorLen = 12
+#sensitivity = 2048             # 2g-16384; 4g-8192; 8g-4096; 16g-2048; according to the datasheet
 
-file = open('testeBT_MPU.csv', 'a')
+file = open('teste.csv', 'a')
 sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 sock.connect((bd_addr, port))
 print('Esp32 connected')
 
-sock.settimeout(1.0)
+sock.settimeout(2.0)
 print('Starting Esp32 acquisition.')
 sock.send("s")
 
 
 while (end - start) < runtime:
-    data = sock.recv(1024)
+    data = sock.recv(2048)
     seconds = int(time.time())
-    #print(seconds,data)
-    print(seconds,',[',data[0],' ',data[1],' ',data[2],' ',data[3],' ',data[4],' ',data[5],']')
-    """
-    print(seconds,',[',data[0],' ',data[1],' ',data[2],' ',data[3],' ',data[4],' ',
-    				   data[5],' ',data[6],' ',data[7],' ',data[8],' ',data[9],' ',
-    				   data[10],' ',data[11],' ',data[12],' ',data[13],' ',data[14],' ',
-    				   data[15],' ',data[16],' ',data[17],' ',data[18],' ',data[19],' ',
-    				   data[20],' ',data[21],' ',data[22],' ',data[23],' ',data[24],' ',
-    				   data[25],' ',data[26],' ',data[27],' ',data[28],' ',data[29],']'
-    				   )
-    """
-    #writer = csv.writer(file,delimiter = ',')
-    #writer.writerow([str(seconds),str(int.from_bytes(data,"little"))])
-    #writer.writerow([str(seconds),str(data.decode("utf-8"))])
-    #writer.writerow([str(seconds),str(data[0]),str(data[1]),str(data[2]),str(data[3])])
     file.write(str(seconds))
-    file.write(",")
-    """
+    file.write(',')
     for x in range(vectorLen):
     	file.write(str(data[x]))
-    	if (x < (vectorLen-1)):
-    		file.write(",")
-    """
-    for x in range(int(vectorLen/2)):
-        acc = (data[x] << 8 | data[x+1])/sensitivity
-        file.write(str(round(acc,2)))
-        if (x < (int(vectorLen/2)-1)):
-            file.write(",")
-        x = x+2
+    	if(x < (vectorLen-1)):
+    		file.write(',')
     file.write("\n")
     end = int(time.time())
 
 print('Stopping Esp32 acquisition.')
-sock.settimeout(1.0)
 sock.send("x")
 sock.close()
 print('Esp32 disconected')
--- Tests/testBT_converted.py
+++ Tests/testBT_converted.py
@@ -9,6 +9,7 @@ import time
 import csv
 import math
 
+# https://www.geeksforgeeks.org/invert-actual-bits-number/
 def invertBits(num):  
   
     # calculating number of bits in the number  
@@ -22,15 +23,20 @@ def invertBits(num):
 
     return value
 
+def twos_complement(input_value: int, num_bits: int) -> int:
+    """Calculates a two's complement integer from the given input value's bits."""
+    mask = 2 ** (num_bits - 1)
+    return -(input_value & mask) + (input_value & ~mask)
+
 start = int(time.time())
 end = int(time.time())
 runtime = 15
-bd_addr = "30:AE:A4:CC:26:12" #itade address
+bd_addr = "C4:4F:33:3E:B3:CB" #top: 30:AE:A4:CC:26:12    low:C4:4F:33:3E:B3:CB
 port = 1
 vectorLen = 6
 sensitivity = 2048          # 2g-16384; 4g-8192; 8g-4096; 16g-2048; according to the datasheet
 
-file = open('testeBT_MPU.csv', 'a')
+file = open('teste.csv', 'a')
 sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 sock.connect((bd_addr, port))
 print('Esp32 connected')
@@ -43,46 +49,30 @@ sock.send("s")
 while (end - start) < runtime:
     data = sock.recv(1024)
     seconds = int(time.time())
-    print(seconds,',[',data[0],' ',data[1],' ',data[2],' ',data[3],' ',data[4],' ',data[5],']')
+    #print(seconds,',[',data[0],' ',data[1],' ',data[2],' ',data[3],' ',data[4],' ',data[5],']')
 
-    file.write(str(seconds))
-    file.write(",")
-
+    #file.write(str(seconds))
+    #file.write(",")
+    """
     for x in range(int(vectorLen/2)):
-        if(data[x] > 127 & data[x+1] > 127):
-            MSB = invertBits(data[x])
-            LSB = invertBits(data[x+1])
-            acc = (MSB << 8 | LSB)/sensitivity
-            #acc = (data[x] << 8 | data[x+1])/sensitivity
-            file.write(str(round(acc,2)))
-
-        if(data[x] > 127 & data[x+1] <= 127):
-            MSB = invertBits(data[x])
-            LSB = data[x+1]
-            acc = (MSB << 8 | LSB)/sensitivity
-            #acc = (data[x] << 8 | data[x+1])/sensitivity
-            file.write(str(round(acc,2)))
-
-        if(data[x] <= 127 & data[x+1] > 127):
-            MSB = data[x]
-            LSB = invertBits(data[x+1])
-            acc = (MSB << 8 | LSB)/sensitivity
-            #acc = (data[x] << 8 | data[x+1])/sensitivity
-            file.write(str(round(acc,2)))
-
-        if(data[x] <= 127 & data[x+1] <= 127):
-            MSB = data[x]
-            LSB = data[x+1]
-            acc = (MSB << 8 | LSB)/sensitivity
-            #acc = (data[x] << 8 | data[x+1])/sensitivity
-            file.write(str(round(acc,2)))
-
+        raw_acc = data[x] << 8 | data[x+1]
+        acc = twos_complement(raw_acc,16) / sensitivity
+        file.write(str(round(acc,2)))
         if (x < (int(vectorLen/2)-1)):
             file.write(",")
-
         x = x+2
-    
-    file.write("\n")
+    """
+    raw_accX = data[0] << 8 | data[1]
+    raw_accY = data[2] << 8 | data[3]
+    raw_accZ = data[4] << 8 | data[5]
+
+    accX = twos_complement(raw_accX,16) / sensitivity
+    accY = twos_complement(raw_accY,16) / sensitivity
+    accZ = twos_complement(raw_accZ,16) / sensitivity
+
+    print("AccX: ", round(accX,2), "   AccY: ", round(accY,2), "   AccZ: ", round(accZ,2))
+    file.write(str(seconds) + "," + str(round(accX,2)) + "," + str(round(accY,2)) + "," + str(round(accZ,2)) + "\n")
+    #file.write("\n")
     end = int(time.time())
 
 print('Stopping Esp32 acquisition.')
