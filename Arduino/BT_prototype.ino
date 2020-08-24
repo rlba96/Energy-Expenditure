01f1dce100574b6df34f6be586daa877b1b41a1e
rlba96 <57870939+rlba96@users.noreply.github.com>
2020-08-24 11:22:39 +0100

    Add files via upload

--- Arduino/BT_prototype.ino
+++ Arduino/BT_prototype.ino
@@ -16,7 +16,7 @@
 #endif
 
 // ICM20648 registers
-#define ICM20648_ADDRESS            0x69
+#define ICM20648_ADDRESS            0x68
 
 #define ICM20648_ACCEL_XOUT_H       0x2D
 #define ICM20648_ACCEL_XOUT_L       0x2E  
@@ -30,14 +30,6 @@
 
 #define ICM20648_WHO_AM_I           0x00
 
-// MAX30205 registers
-#define MAX30205_ADDRESS            0x48
-
-#define MAX30205_TEMPERATURE        0x00
-#define MAX30205_CONFIGURATION      0x01
-#define MAX30205_THYST              0x02
-#define MAX30205_TOS                0x03
-
 // TCA9558A register
 #define TCAADDR                     0x70
 
@@ -46,7 +38,7 @@
 #define I2C_SDA                     21
 #define I2C_SCL                     22
 
-#define VECTOR_LENGTH 8
+#define VECTOR_LENGTH 36
 
 // Bluetooth Serial object
 BluetoothSerial SerialBT;
@@ -57,8 +49,8 @@ char incomingChar;
 bool deviceConnected;
 
 // ICM-20648 variables
-//const int ICM_I2C_ADRESS = 0x68;      // ICM20648 address
 uint8_t data_over_BT[VECTOR_LENGTH];      // Byte array to send over bluetooth
+
 
 // Function to select the TCA9548 Switch channel
 void TCA_select(uint8_t channel) {
@@ -71,11 +63,27 @@ void TCA_select(uint8_t channel) {
   Wire.endTransmission();  
 }
 
+
 void setup() {
     Serial.begin(115200);
     //Wire.setClock(400000);  // I2C fast mode
     
-    // Initialize the first sensor
+    // Initialize the ICM20648 sensor
+    // Channel 0
+    Wire.begin(I2C_SDA, I2C_SCL);
+    TCA_select(0);
+    Wire.beginTransmission(ICM20648_ADDRESS);
+    Wire.write(ICM20648_PWR_MGMT_1);           // Accessing Power Management register
+    Wire.write(0x00);                          // Setting SLEEP register to 0 (Required)
+    Wire.endTransmission();
+            
+    Wire.beginTransmission(ICM20648_ADDRESS);
+    Wire.write(ACCEL_CONFIG);                  // Accessing Acceleration Configuration register
+    Wire.write(0b00000110);                    // Set full scale range to +-16g (bit 1 and 2)
+    Wire.endTransmission(true);                // true
+    delay(20);
+
+    // Channel 1
     Wire.begin(I2C_SDA, I2C_SCL);
     TCA_select(1);
     Wire.beginTransmission(ICM20648_ADDRESS);
@@ -86,9 +94,65 @@ void setup() {
     Wire.beginTransmission(ICM20648_ADDRESS);
     Wire.write(ACCEL_CONFIG);                  // Accessing Acceleration Configuration register
     Wire.write(0b00000110);                    // Set full scale range to +-16g (bit 1 and 2)
-    Wire.endTransmission();                // true
+    Wire.endTransmission(true);                // true
     delay(20);
-
+    
+    // Channel 2
+    Wire.begin(I2C_SDA, I2C_SCL);
+    TCA_select(2);
+    Wire.beginTransmission(ICM20648_ADDRESS);
+    Wire.write(ICM20648_PWR_MGMT_1);           // Accessing Power Management register
+    Wire.write(0x00);                          // Setting SLEEP register to 0 (Required)
+    Wire.endTransmission();
+            
+    Wire.beginTransmission(ICM20648_ADDRESS);
+    Wire.write(ACCEL_CONFIG);                  // Accessing Acceleration Configuration register
+    Wire.write(0b00000110);                    // Set full scale range to +-16g (bit 1 and 2)
+    Wire.endTransmission(true);                // true
+    delay(20);
+    
+    // Channel 4
+    Wire.begin(I2C_SDA, I2C_SCL);
+    TCA_select(4);
+    Wire.beginTransmission(ICM20648_ADDRESS);
+    Wire.write(ICM20648_PWR_MGMT_1);           // Accessing Power Management register
+    Wire.write(0x00);                          // Setting SLEEP register to 0 (Required)
+    Wire.endTransmission();
+            
+    Wire.beginTransmission(ICM20648_ADDRESS);
+    Wire.write(ACCEL_CONFIG);                  // Accessing Acceleration Configuration register
+    Wire.write(0b00000110);                    // Set full scale range to +-16g (bit 1 and 2)
+    Wire.endTransmission(true);                // true
+    delay(20);
+    
+    // Channel 5
+    Wire.begin(I2C_SDA, I2C_SCL);
+    TCA_select(5);
+    Wire.beginTransmission(ICM20648_ADDRESS);
+    Wire.write(ICM20648_PWR_MGMT_1);           // Accessing Power Management register
+    Wire.write(0x00);                          // Setting SLEEP register to 0 (Required)
+    Wire.endTransmission();
+            
+    Wire.beginTransmission(ICM20648_ADDRESS);
+    Wire.write(ACCEL_CONFIG);                  // Accessing Acceleration Configuration register
+    Wire.write(0b00000110);                    // Set full scale range to +-16g (bit 1 and 2)
+    Wire.endTransmission(true);                // true
+    delay(20);
+    
+    // Channel 6
+    Wire.begin(I2C_SDA, I2C_SCL);
+    TCA_select(6);
+    Wire.beginTransmission(ICM20648_ADDRESS);
+    Wire.write(ICM20648_PWR_MGMT_1);           // Accessing Power Management register
+    Wire.write(0x00);                          // Setting SLEEP register to 0 (Required)
+    Wire.endTransmission();
+            
+    Wire.beginTransmission(ICM20648_ADDRESS);
+    Wire.write(ACCEL_CONFIG);                  // Accessing Acceleration Configuration register
+    Wire.write(0b00000110);                    // Set full scale range to +-16g (bit 1 and 2)
+    Wire.endTransmission(true);                // true
+    delay(20);
+    
     SerialBT.begin("ESP32");
     
     if(!SerialBT.begin("ESP32"))
@@ -101,13 +165,13 @@ void setup() {
 
 void loop() {
     if(deviceConnected){
-
       // Read ICM-20648 data
+      // Channel 0
       Wire.begin(I2C_SDA, I2C_SCL);
-      TCA_select(1);
+      TCA_select(0);
       Wire.beginTransmission(ICM20648_ADDRESS);
       Wire.write(ICM20648_ACCEL_XOUT_H);
-      Wire.endTransmission();             //false
+      Wire.endTransmission(false);             //false
       Wire.requestFrom(ICM20648_ADDRESS, 6, true);
       data_over_BT[0] = Wire.read();
       data_over_BT[1] = Wire.read();
@@ -116,6 +180,76 @@ void loop() {
       data_over_BT[4] = Wire.read();
       data_over_BT[5] = Wire.read();
 
+      // Channel 1
+      Wire.begin(I2C_SDA, I2C_SCL);
+      TCA_select(1);
+      Wire.beginTransmission(ICM20648_ADDRESS);
+      Wire.write(ICM20648_ACCEL_XOUT_H);
+      Wire.endTransmission(false);             //false
+      Wire.requestFrom(ICM20648_ADDRESS, 6, true);
+      data_over_BT[6] = Wire.read();
+      data_over_BT[7] = Wire.read();
+      data_over_BT[8] = Wire.read();
+      data_over_BT[9] = Wire.read();
+      data_over_BT[10] = Wire.read();
+      data_over_BT[11] = Wire.read();
+      
+      // Channel 2
+      Wire.begin(I2C_SDA, I2C_SCL);
+      TCA_select(2);
+      Wire.beginTransmission(ICM20648_ADDRESS);
+      Wire.write(ICM20648_ACCEL_XOUT_H);
+      Wire.endTransmission(false);             //false
+      Wire.requestFrom(ICM20648_ADDRESS, 6, true);
+      data_over_BT[12] = Wire.read();
+      data_over_BT[13] = Wire.read();
+      data_over_BT[14] = Wire.read();
+      data_over_BT[15] = Wire.read();
+      data_over_BT[16] = Wire.read();
+      data_over_BT[17] = Wire.read();
+            
+      // Channel 4
+      Wire.begin(I2C_SDA, I2C_SCL);
+      TCA_select(4);
+      Wire.beginTransmission(ICM20648_ADDRESS);
+      Wire.write(ICM20648_ACCEL_XOUT_H);
+      Wire.endTransmission(false);             //false
+      Wire.requestFrom(ICM20648_ADDRESS, 6, true);
+      data_over_BT[18] = Wire.read();
+      data_over_BT[19] = Wire.read();
+      data_over_BT[20] = Wire.read();
+      data_over_BT[21] = Wire.read();
+      data_over_BT[22] = Wire.read();
+      data_over_BT[23] = Wire.read();
+                  
+      // Channel 5
+      Wire.begin(I2C_SDA, I2C_SCL);
+      TCA_select(5);
+      Wire.beginTransmission(ICM20648_ADDRESS);
+      Wire.write(ICM20648_ACCEL_XOUT_H);
+      Wire.endTransmission(false);             //false
+      Wire.requestFrom(ICM20648_ADDRESS, 6, true);
+      data_over_BT[24] = Wire.read();
+      data_over_BT[25] = Wire.read();
+      data_over_BT[26] = Wire.read();
+      data_over_BT[27] = Wire.read();
+      data_over_BT[28] = Wire.read();
+      data_over_BT[29] = Wire.read();
+                        
+      // Channel 6
+      Wire.begin(I2C_SDA, I2C_SCL);
+      TCA_select(6);
+      Wire.beginTransmission(ICM20648_ADDRESS);
+      Wire.write(ICM20648_ACCEL_XOUT_H);
+      Wire.endTransmission(false);             //false
+      Wire.requestFrom(ICM20648_ADDRESS, 6, true);
+      data_over_BT[30] = Wire.read();
+      data_over_BT[31] = Wire.read();
+      data_over_BT[32] = Wire.read();
+      data_over_BT[33] = Wire.read();
+      data_over_BT[34] = Wire.read();
+      data_over_BT[35] = Wire.read();
+/*
       Serial.print(data_over_BT[0]);
       Serial.print(" ");
       Serial.print(data_over_BT[1]);
@@ -128,27 +262,10 @@ void loop() {
       Serial.print(" ");
       Serial.print(data_over_BT[5]);  
       Serial.println("");
-
-      //Wire.begin(I2C_SDA, I2C_SCL);
-      Wire.begin();
-      TCA_select(0);
-      Wire.beginTransmission(MAX30205_ADDRESS);
-      Wire.write(MAX30205_TEMPERATURE);
-      Wire.endTransmission(false);             
-      Wire.requestFrom(MAX30205_ADDRESS, 2);
-      data_over_BT[6] = Wire.read();
-      data_over_BT[7] = Wire.read();
-
-      Serial.print(data_over_BT[6]);
-      Serial.print(" ");
-      Serial.print(data_over_BT[7]);  
-      Serial.println("");
-
+*/
       // Send value
       SerialBT.write(data_over_BT,VECTOR_LENGTH);
       Serial.println("\n");
-
-      delay(500);
     }  
 
     // Read received messages (to open and close connection)
@@ -160,7 +277,6 @@ void loop() {
       else{
         message = "";
       }
-      
       if (message == "s"){
         deviceConnected = true;
         message = "";
