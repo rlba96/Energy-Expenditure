64358deac0af73acacc4a09a129920d6d6c8ab58
rlba96 <57870939+rlba96@users.noreply.github.com>
2020-12-02 10:41:37 +0000

    Update and rename testBT.py to test_BT.py

--- Tests/test_BT.py
+++ Tests/test_BT.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 """
-A simple Python script to receive messages from a client over 
-Bluetooth using PyBluez (with Python 2). 
-
-print(seconds,data[7])
-print(seconds,',[',data[0],' ',data[1],' ',data[2],' ',data[3],' ',data[4],' ',data[5],' ',data[6],' ',data[7],' ',data[8],' ',data[9],' ',data[10],' ',data[11],' ]')
-    
-
+A simple Python script to receive messages from a client over Bluetooth using PyBluez. 
 """
 import bluetooth
 import sys
@@ -20,7 +14,6 @@ runtime = 10
 bd_addr = "C4:4F:33:3E:B3:CB"   #top: 30:AE:A4:CC:26:12    low:C4:4F:33:3E:B3:CB
 port = 1
 vectorLen = 12
-#sensitivity = 2048             # 2g-16384; 4g-8192; 8g-4096; 16g-2048; according to the datasheet
 
 file = open('teste.csv', 'a')
 sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
@@ -30,7 +23,6 @@ print('Esp32 connected')
 sock.settimeout(2.0)
 print('Starting Esp32 acquisition.')
 sock.send("s")
-
 
 while (end - start) < runtime:
     data = sock.recv(2048)
