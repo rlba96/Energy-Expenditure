802d5e3a00ce4b271f248fb6fdab3df86e0ffeb3
rlba96 <57870939+rlba96@users.noreply.github.com>
2020-12-02 10:17:29 +0000

    Delete mergeCSV.py

--- LTS_18.04/iot_workshop/mergeCSV.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import pandas as pd
-
-d_one = pd.read_csv('Dataset_Bitalino.csv',sep=',',engine='python',header=0)
-d_two = pd.read_csv('Dataset_MPU6050.csv',sep=',',engine='python',header=0)
-d_three = pd.merge(d_one, d_two, left_on='timestamp',right_on='timestamp')
-d_three.to_csv('output.csv',sep=',', index=False)
